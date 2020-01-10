3bebba86d99c15b26b8c8095cc73652d00c52410
Magdalena Chmura <magdalena.chmura24@gmail.com>
2020-01-10 21:44:58 +0100

    Add description to multiple stream

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 import { of } from 'rxjs';   
 import { map } from 'rxjs/operators';
 
-let streamA  = of(3, 4);
-let streamB = streamA.pipe(
+// Stream to multiple values. 
+
+// Stream allows you to specify the dynamic behaviour of a value only at the time of declaration.
+// If you want to multiple more value you could add them only in stream declaration. 
+let multiple = () => {
+  let streamA  = of(3, 4, 5);
+  let streamB = streamA.pipe(
   map( x => x * 10)
 )
+  streamB.subscribe(x => console.log(x));
 
-streamB.subscribe(x => console.log(x))
+}
