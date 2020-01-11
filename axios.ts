3a8eae20f9a66e8b085b41b3e405da4693331d80
Magdalena Chmura <magdalena.chmura24@gmail.com>
2020-01-11 10:30:05 +0100

    Node request with rxjs

--- axios.ts
+++ axios.ts
@@ -1,11 +1,15 @@
-import { of, from} from 'rxjs';   
-import { map, flatMap } from 'rxjs/operators';
+import { of, from, interval} from 'rxjs';   
+import { map, mergeMap} from 'rxjs/operators';
 import axios from "axios";
+
+// Promises are simplyfy version of Observable that can thave only one value: resolve or error.
+// Observable can have a multiple events.
+// from: Promise => Observable 
 
 let requestStream = of('https://api.github.com/users');
 
 let responseStream = requestStream.pipe(
-  flatMap(requestURL => from(axios.get(requestURL)))
+  mergeMap(requestURL => from(axios.get(requestURL)))
 )
 
 responseStream.subscribe(response => {
